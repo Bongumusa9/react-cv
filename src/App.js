d23f20a9a229151fba16d3d70955e984cb230560
PU_MAC_11 <bongz.mlaba@gmail.com>
2023-05-19 15:36:01 +0200

    Added scrollup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Qualification from "./components/qualification/Qualification";
+import ScrollUp from "./components/scrollup/ScrollUp";
 import Skills from "./components/skills/Skills";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <ScrollUp/>
     </>
   );
 }
